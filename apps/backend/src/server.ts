5f82630f47414c6a71ebc3c321ce4a36ea4cc146
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-03-20 03:08:31 -0300

    feat(backend): create users router

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,10 @@
 import Fastify from "fastify";
+import { usersRoutes } from "@/routes/users";
 
 export function server() {
     const app = Fastify();
+
+    app.register(usersRoutes);
 
     return app;
 }
