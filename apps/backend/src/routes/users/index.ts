30fb53a40417d88feaa5e55eff9e939968a9ef34
Grazielly Varanda <vieiravarandagrazielly@gmail.com>
2024-04-02 21:47:03 -0300

    feat(backend): add post user route

--- apps/backend/src/routes/users/index.ts
+++ apps/backend/src/routes/users/index.ts
@@ -1,10 +1,36 @@
 import { prisma } from "@/utils/prisma";
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { createUserSchema } from "./schema/create-user";
+import { z } from "zod";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.get("/users", async (request: FastifyRequest, reply: FastifyReply) => {
         const users = await prisma.user.findMany();
 
         return users;
+    });
+
+    app.post("/users", async (request: FastifyRequest, reply: FastifyReply) => {
+        try {
+            const { email, name } = createUserSchema.parse(request.body);
+
+            await prisma.user.create({
+                data: {
+                    email,
+                    name
+                }
+            });
+
+            return reply.code(201).send({
+                message: "Criado com sucesso"
+            });
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                reply.status(400).send({ message: err });
+                return;
+            }
+
+            console.log(`Erro no servidor: ${err}`);
+        }
     });
 }
