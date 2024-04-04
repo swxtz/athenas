84bd6d097b88b2f5186ad2dd7aeec21b87fcc1fb
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-04-04 20:21:25 -0300

    feat(backend): add fastify jwt module

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,10 +1,17 @@
 import Fastify from "fastify";
 import { usersRoutes } from "@/routes/users/users";
+import fastifyJWT from "@fastify/jwt";
+import { env } from "./utils/env";
 
 export function server() {
     const app = Fastify();
 
+    // Modules
+    app.register(fastifyJWT, { secret: env.jwtSecret });
+
+    // Routes
     app.register(usersRoutes);
+    
 
     return app;
 }
--- apps/backend/src/utils/env.ts
+++ apps/backend/src/utils/env.ts
@@ -4,12 +4,14 @@ const schema = z.object({
     PORT: z.coerce.number(),
     DATABASE_URL: z.string(),
     DEV_ENV: z.coerce.boolean(),
+    JWT_SECRET: z.string(),
 });
 
-const { DATABASE_URL, PORT, DEV_ENV } = schema.parse(process.env);
+const { DATABASE_URL, PORT, DEV_ENV, JWT_SECRET } = schema.parse(process.env);
 
 export const env = {
     port: PORT,
     databaseUrl: DATABASE_URL,
-    dev: DEV_ENV
+    dev: DEV_ENV,
+    jwtSecret: JWT_SECRET,
 };
