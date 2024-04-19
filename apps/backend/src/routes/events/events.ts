21f955ecb75e167d0306496b007a0f944208f712
Rafael Raposo <rrafa7124@gmail.com>
2024-04-18 22:28:15 -0300

    feat(backend): create post events route

--- apps/backend/src/routes/events/events.ts
+++ apps/backend/src/routes/events/events.ts
@@ -1,3 +1,49 @@
-import fastify from "f
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { postEventSchema } from "./schemas/post-event";
+import { z } from "zod";
+import { prisma } from "@/utils/prisma";
 
-export async function eventsRoutes(app: Fat)
+
+export async function eventsRoutes(app: FastifyInstance) {
+    app.post("/", async (req: FastifyRequest, reply: FastifyReply) => {
+        //autenticação
+
+        try {
+            const { date, imageUrl, location, mainAttraction, name, price } = postEventSchema.parse(req.body);
+
+            const verifyEvent = await prisma.event.findUnique({
+                where: { name: name }
+            });
+
+            if (verifyEvent) {
+                return reply.code(400).send({ message: "Este nome já está sendo utilizado" });
+
+            }
+
+            const event = await prisma.event.create({
+                data: {
+                    date,
+                    imageUrl,
+                    location,
+                    mainAttraction,
+                    name,
+                    price,
+                }
+            });
+
+
+            return reply.code(201).send({ message: "Evento criado com sucesso", data: { ...event } });
+
+
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                reply.status(400).send({ message: err });
+                return;
+            }
+
+            console.log(`Erro no servidor: ${err}`);
+        }
+    });
+
+
+}
--- apps/backend/src/routes/events/schemas/post-event.ts
+++ apps/backend/src/routes/events/schemas/post-event.ts
@@ -1,3 +1,11 @@
 import { z } from "zod";
 
-export const postEventSchema = z
+export const postEventSchema = z.object({
+    name: z.string().min(2, "O nome deve ter no mínimo 2 caracteres").max(255, "O nome deve ter no máximo 255 caracteres"),
+    price: z.number().min(0, "O valor deve ser positivo"),
+    mainAttraction: z.string().min(2, "A atração deve ter no mínimo 2 caracteres").max(500, "A atração deve ter no máximo 500 caracteres"),
+    imageUrl: z.string().url("A imagem tem que ser uma URL"),
+    location: z.string(),
+    date: z.string().datetime({ message: "A data precisa ser DateTime" }),
+
+});
