ab9bb1a5ba8a78da1f2820615ede544ce49b6093
Rafael Raposo <rrafa7124@gmail.com>
2024-04-18 22:28:15 -0300

    feat(backend): create post events route

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify";
 import { usersRoutes } from "@/routes/users/users";
 import fastifyJWT from "@fastify/jwt";
 import { env } from "./utils/env";
+import { eventsRoutes } from "./routes/events/events";
 
 export function server() {
     const app = Fastify();
@@ -10,7 +11,8 @@ export function server() {
     app.register(fastifyJWT, { secret: env.jwtSecret });
 
     // Routes
-    app.register(usersRoutes);
+    app.register(usersRoutes); 
+    app.register(eventsRoutes, { prefix: "/events" });
     
 
     return app;
