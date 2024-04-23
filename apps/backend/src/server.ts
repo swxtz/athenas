c8654566504a7895dad2c7031830b986a9161494
Rafael Raposo <rrafa7124@gmail.com>
2024-04-23 17:14:39 -0300

    feat(backend): create auth route

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,6 +3,7 @@ import { usersRoutes } from "@/routes/users/users";
 import fastifyJWT from "@fastify/jwt";
 import { env } from "./utils/env";
 import { eventsRoutes } from "./routes/events/events";
+import { authRoutes } from "./routes/auth/auth";
 
 export function server() {
     const app = Fastify();
@@ -13,6 +14,7 @@ export function server() {
     // Routes
     app.register(usersRoutes);
     app.register(eventsRoutes, { prefix: "/events" });
+    app.register(authRoutes, { prefix: "/auth" });
 
 
     return app;
