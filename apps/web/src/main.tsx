17b15801e2170cffc64ae5d2c394d7d82fd2a4f0
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-04-24 02:35:18 -0300

    feat(web): add shadcn/ui

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routerObject } from "./libs/router";
+import { routerObject } from "./pages/router";
 
 const router = createBrowserRouter(routerObject);
 
