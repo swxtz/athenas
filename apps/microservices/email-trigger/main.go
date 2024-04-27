4cf56411361164d390920673e5dacd70fa0be73d
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-04-26 21:49:22 -0300

    feat(email-trigger): add gin gonic

--- apps/microservices/email-trigger/main.go
+++ apps/microservices/email-trigger/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/swxtz/athenas/apps/microservices/email-trigger/api"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	api.Handler()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
