0d6ebf65a18824ed8ede604e3e9ff2359f360283
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-05-13 20:02:02 -0300

    feat(web): add animate show carousel

--- apps/web/src/pages/home/components/carousel.tsx
+++ apps/web/src/pages/home/components/carousel.tsx
@@ -7,11 +7,11 @@ import {
 import weekendBanner from "@/assets/images/events/weekend.png";
 import kendrickLamarBanner from "@/assets/images/events/kendrick-lamar.png";
 import futureMetroBanner from "@/assets/images/events/future-metro-booming.png";
-import tylerTheCreatorBanner from "@/assets/images/events/tyler-the-creator.png";  
+import tylerTheCreatorBanner from "@/assets/images/events/tyler-the-creator.png";
 
 import { uuid } from "@/lib/uuid";
 import { Link } from "react-router-dom";
-import { Calendar, MapPin } from "lucide-react";
+import { CarouselInfoLabel } from "@/components/carousel-info-label";
 
 interface CarouselDisplayItem {
   title: string;
@@ -61,30 +61,18 @@ export function Carousel() {
             key={uuid()}
             className="flex flex-col gap-8 w-[90%] mt-8 md:mt-12"
           >
-            <Link to={item.linkToEvent} className="w-[90%] mx-auto md:w-full">
+            <Link to={item.linkToEvent} className="w-[90%] mx-auto md:w-full 2xl:w-3/5">
               <img
                 className="w-full rounded-3xl"
                 src={item.imageUrl}
                 alt={item.title}
               />
             </Link>
-            <div className="flex flex-row items container gap-4 justify-between md:px-16">
-              <span className="font-medium text-1xl md:text-4xl">
-                {item.title}
-              </span>
-              <div className="">
-                <span className="font-medium text-zinc-400 text-xs md:text-xl flex flex-row items-center gap-2">
-                  <MapPin className="size-4" /> <span>{item.location}</span>
-                </span>
-                <span className="font-medium text-zinc-400 text-xs md:text-xl flex flex-row items-center gap-2">
-                  <Calendar className="size-4" /> <span>{new Intl.DateTimeFormat("pt-BR", {
-                    dateStyle: "medium",
-                    timeStyle: "short"
-                  }).format(item.date)}</span>
-                </span>
-              </div>
-
-            </div>
+            <CarouselInfoLabel
+              date={item.date}
+              location={item.location}
+              title={item.title}
+            />
           </CarouselItem>
         ))}
       </CarouselContent>
