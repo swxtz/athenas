3564f766282e803b94dadd6d1ed9d2f44f35d045
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-06-11 20:27:26 -0300

    feat: add swagger

--- apps/backend/dist/main.js
+++ apps/backend/dist/main.js
@@ -4,8 +4,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 const _core = require("@nestjs/core");
 const _appmodule = require("./app.module");
+const _swagger = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await _core.NestFactory.create(_appmodule.AppModule);
+    const swaggerConfig = new _swagger.DocumentBuilder().setTitle("Athenas").setDescription("API do ecommerce da RN Distriuidora").setVersion("0.0.0").addTag("e-commerce").build();
+    const document = _swagger.SwaggerModule.createDocument(app, swaggerConfig);
+    _swagger.SwaggerModule.setup("api-docs", app, document);
     await app.listen(3000);
 }
 bootstrap();
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Athenas")
+    .setDescription("API do ecommerce da RN Distriuidora")
+    .setVersion("0.0.0")
+    .addTag("e-commerce")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api-docs", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
