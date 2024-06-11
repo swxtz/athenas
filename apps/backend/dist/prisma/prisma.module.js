924463084458eac71088123d202ae8c68a797348
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-06-11 20:36:41 -0300

    feat: add prisma

--- apps/backend/dist/prisma/prisma.module.js
+++ apps/backend/dist/prisma/prisma.module.js
@@ -2,38 +2,32 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-Object.defineProperty(exports, "AppModule", {
+Object.defineProperty(exports, "PrismaModule", {
     enumerable: true,
     get: function() {
-        return AppModule;
+        return PrismaModule;
     }
 });
 const _common = require("@nestjs/common");
-const _appcontroller = require("./app.controller");
-const _appservice = require("./app.service");
-const _prismaservice = require("./prisma/prisma.service");
-const _prismamodule = require("./prisma/prisma.module");
+const _prismaservice = require("./prisma.service");
 function _ts_decorate(decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for(var i = decorators.length - 1; i >= 0; i--)if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 }
-let AppModule = class AppModule {
+let PrismaModule = class PrismaModule {
 };
-AppModule = _ts_decorate([
+PrismaModule = _ts_decorate([
+    (0, _common.Global)(),
     (0, _common.Module)({
-        imports: [
-            _prismamodule.PrismaModule
-        ],
-        controllers: [
-            _appcontroller.AppController
-        ],
         providers: [
-            _appservice.AppService,
+            _prismaservice.PrismaService
+        ],
+        exports: [
             _prismaservice.PrismaService
         ]
     })
-], AppModule);
+], PrismaModule);
 
-//# sourceMappingURL=app.module.js.map
+//# sourceMappingURL=prisma.module.js.map
