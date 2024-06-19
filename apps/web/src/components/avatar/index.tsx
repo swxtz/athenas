20d31b4c917b07afb58a7abd1af5df89de95e7e4
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-06-19 20:44:06 -0300

    feat: create user avatar

---
+++ apps/web/src/components/avatar/index.tsx
@@ -0,0 +1,29 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import {
+  Avatar as AvatarRoot,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import { User } from "lucide-react";
+
+interface AvatarProps {
+  name?: string;
+  email?: string;
+  avatarPhotoUrl?: string | StaticImport;
+}
+
+export function Avatar({ name, email, avatarPhotoUrl }: AvatarProps) {
+  return (
+    <div className="">
+      <div className="">
+        <AvatarRoot>
+          <AvatarImage src="https://github.com/aasddsadsadsdasasdas.png" />
+          <AvatarFallback className="">
+            <User className="text-zinc-900 p-1 w-96" size={128} />
+          </AvatarFallback>
+        </AvatarRoot>
+      </div>
+    </div>
+  );
+}
--- apps/web/src/components/navbar/index.tsx
+++ apps/web/src/components/navbar/index.tsx
@@ -7,9 +7,12 @@ import {
   Dialog,
   DialogPortal,
   DialogTrigger,
-  DialogContent
+  DialogContent,
+  DialogOverlay,
 } from "@radix-ui/react-dialog";
-import { DialogOverlay } from "../ui/dialog";
+import { Avatar } from "../avatar";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export function Navbar() {
   return (
@@ -30,9 +33,23 @@ export function Navbar() {
           </DialogTrigger>
 
           <DialogPortal>
-            <DialogOverlay className="fixed inset-0 bg-white/40" />
-            <DialogContent className="fixed p-10 m-0 right-0 top-0 bottom-0 h-screen w-3/4 md:w-1/2 lg:w-1/5 bg-slate-200 border-l border-zinc-900 container data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-right-1/2 data-[state=open]:slide-in-from-right-1/2">
-              <p>conteudo...</p>
+            <DialogOverlay className="fixed inset-0 bg-white/5" />
+            <DialogContent className="fixed p-10 m-0 right-0 top-0 bottom-0 h-screen w-3/4 md:w-1/2 lg:w-[350px] bg-slate-200 border-l border-zinc-900 container data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-right-1/2 data-[state=open]:slide-in-from-right-1/2">
+              <div className="">
+                <div className="flex w-full items-center gap-4">
+                  <Avatar />
+                  <div className="">
+                    <span>
+                      Faça <span className="font-semibold">login</span> ou{" "}
+                      <span className="font-semibold">crie</span> sua conta
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex justify-center mt-8 ">
+                  <Link href="/auth/login" className="bg-zinc-900 text-zinc-50 w-full md:text-base flex items-center justify-center rounded py-1 text-sm">Login</Link>
+                </div>
+              </div>
             </DialogContent>
           </DialogPortal>
         </Dialog>
