d316fa1c3fddd59c0b5c253b8e86c514bf8b7634
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-07-04 21:22:18 -0300

    infra: create terraform files to ec2 instance

---
+++ apps/backend/infra/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "The name of the EC2 instance"
+  default     = "athenas-backend-server"
+}
+
+variable "env" {
+  description = "The environment of the EC2 instance"
+  default     = "dev"
+}
+
+variable "repo" {
+  description = "The source of the EC2 instance"
+  default     = "https://github.com/swxtz/athenas"
+}
+
+variable "ami" {
+  description = "The AMI to use for the EC2 instance"
+  default     = "ami-080111c1449900431"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to launch"
+  default     = "t2.micro"
+}
