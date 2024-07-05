b23b8e9220bb9daf994c8677f4889d27008fedc3
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-07-04 22:22:42 -0300

    infra: add key pair in ec2 instance

--- apps/backend/infra/ec2.tf
+++ apps/backend/infra/ec2.tf
@@ -1,6 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  # filter {
+  #   name   = "virtualization-type"
+  #   values = ["hvm"]
+  # }
+
+  owners = ["099720109477"] # Canonical
+
+}
+
+
 resource "aws_instance" "server" {
-  ami           = var.ami
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
+  key_name = var.key_name
 
   tags = {
     Name        = var.name
--- apps/backend/infra/variables.tf
+++ apps/backend/infra/variables.tf
@@ -22,3 +22,8 @@ variable "instance_type" {
   description = "The type of EC2 instance to launch"
   default     = "t2.micro"
 }
+
+variable "key_name" {
+  description = "The key pair to use for the EC2 instance"
+  default     = "athenas-server"
+}
