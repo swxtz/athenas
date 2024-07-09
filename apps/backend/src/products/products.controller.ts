042f26c65549b5f9e0248f1cf1ae287f476f8440
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-07-09 17:19:37 -0300

    feat: create get all products endpoint

--- apps/backend/src/products/products.controller.ts
+++ apps/backend/src/products/products.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 
 @Controller("products")
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
+
+    @Get("get-all")
+    async getAllProducts() {
+        return this.productsService.getAllProducts();
+    }
 }
--- apps/backend/src/products/products.service.ts
+++ apps/backend/src/products/products.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(private prisma: PrismaService) {}
+
+    async getAllProducts() {
+        return await this.prisma.product.findMany();
+    }
+}
