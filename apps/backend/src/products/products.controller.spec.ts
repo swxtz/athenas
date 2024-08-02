e914911a5ece546a8fccd9031c14d6494fda371d
Rafael Raposo <rrafa7124@gmail.com>
2024-08-02 17:57:39 -0300

    start products tests

--- apps/backend/src/products/products.controller.spec.ts
+++ apps/backend/src/products/products.controller.spec.ts
@@ -16,5 +16,13 @@ describe("ProductsController", () => {
 
     it("should be defined", () => {
         expect(controller).toBeDefined();
+        expect(ProductsService).toBeDefined();
+    });
+
+    describe('get-all', () => {
+        it('should return a product entity sucessfully', async () =>{
+            const result = await ProductsController.getAllProducts();
+            expect(result).toEqual([]);
+        }); 
     });
 });
