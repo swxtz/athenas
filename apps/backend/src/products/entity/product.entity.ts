233d66c49ae7a495a640aee6cb36c9e7328c78be
Rafael Rapozzo <rrafa7124@gmail.com>
2024-08-05 22:47:26 -0300

    feat: create product entity

--- apps/backend/src/products/entity/product.entity.ts
+++ apps/backend/src/products/entity/product.entity.ts
@@ -1,15 +1,24 @@
+import { $Enums } from "@prisma/client";
 export class ProductEntity {
     id?: string;
 
     name: string;
     description: string;
     barcode: string;
-    image: string;
-
+    images: string[];
+    coverImage: string;
+    isAvailable: boolean;
+    rating: string;
+    productType: $Enums.ProductType;
+    state: string;
+    localPickup: boolean;
+    numberOfSales: number;
+    numberOfViews: number;
+    numberOfViewsInLastWeek: number;
     buyPrice: number;
     price: number;
-
     stock: number;
+
     createdAt?: Date;
     updatedAt?: Date;
 
--- apps/backend/src/products/products.controller.spec.ts
+++ apps/backend/src/products/products.controller.spec.ts
@@ -3,6 +3,8 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { ProductEntity } from "./entity/product.entity";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
+import { createId } from "@paralleldrive/cuid2";
+import { date } from "zod";
 
 describe("ProductsController", () => {
     let controller: ProductsController;
@@ -15,13 +17,15 @@ describe("ProductsController", () => {
                 {
                     provide: ProductsService,
                     useValue: {
-                        getAllProducts: jest.fn().mockResolvedValue(ProductEntity),
+                        getAllProducts: jest
+                            .fn()
+                            .mockResolvedValue(ProductEntity),
                         createProduct: jest.fn(),
                         uploadCoverImage: jest.fn(),
                     },
                 },
 
-            JwtService
+                JwtService,
             ],
         }).compile();
 
@@ -29,16 +33,40 @@ describe("ProductsController", () => {
         service = module.get<ProductsService>(ProductsService);
     });
 
+    const product: ProductEntity[] = [
+        new ProductEntity({
+            id: createId(),
+            name: "batata",
+            description: "batata gostosa",
+            barcode: "123456789",
+            images: ["www.google.com"],
+            coverImage: "www.google.com",
+            isAvailable: true,
+            rating: "5",
+            productType: "others",
+            state: "",
+            localPickup: true,
+            numberOfSales: 2,
+            numberOfViews: 10,
+            numberOfViewsInLastWeek: 10,
+            buyPrice: 5,
+            price: 2,
+            stock: 10000,
+
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }),
+    ];
+
     it("should be defined", () => {
         expect(controller).toBeDefined();
         expect(service).toBeDefined();
     });
 
     describe("getAllProducts", () => {
-        it("should return a product entity successfully", async () =>{
+        it("should return a product entity successfully", async () => {
             const result = await controller.getAllProducts();
             expect(result).toEqual([]);
-            
         });
     });
 });
