b2a59896d2479dd341ef8ac22d7d31dfee6895a0
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-08-08 21:34:30 -0300

    test: checking return of recommendation values

--- apps/backend/src/recommentation-values/recommentation-values.service.spec.ts
+++ apps/backend/src/recommentation-values/recommentation-values.service.spec.ts
@@ -17,4 +17,26 @@ describe("RecommentationValuesService", () => {
     it("should be defined", () => {
         expect(service).toBeDefined();
     });
+
+    describe("recommendation values", () => {
+        it("should return view value", () => {
+            expect(RecommentationValuesService.view).toBe(0.03);
+        });
+
+        it("should return sale value", () => {
+            expect(RecommentationValuesService.sale).toBe(0.5);
+        });
+
+        it("should return reBuy value", () => {
+            expect(RecommentationValuesService.reBuyValue).toBe(0.6);
+        });
+
+        it("should return like value", () => {
+            expect(RecommentationValuesService.likeValue).toBe(0.3);
+        });
+
+        it("should return self search value", () => {
+            expect(RecommentationValuesService.selfSearchValue).toBe(0.75);
+        });
+    });
 });
