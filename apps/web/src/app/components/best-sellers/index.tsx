c028d1d444575662f3b84bc08d15e8f7b7c8b9ca
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-08-18 02:11:08 -0300

    style: fix gap in y axis on best sellers section

--- apps/web/src/app/components/best-sellers/index.tsx
+++ apps/web/src/app/components/best-sellers/index.tsx
@@ -76,7 +76,7 @@ export function BestSellers() {
         {/* Divisor */}
         <CategoryDivisor title="Mais vendidos" />
         <div className="mt-6 mx-auto">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-1 lg:grid-cols-6 md:gap-4  mx-auto">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-4 lg:grid-cols-6  gap-y-4 md:gap-y-8 mx-auto">
             {products.map((product) => (
               <ProductCard
                 key={cuid()}
