b2796271ed9c5f15875bdee9414cefd8780cc32a
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-08-21 06:22:22 -0300

    feat: create mocked product info route

---
+++ apps/web/src/app/api/mock/products/get-product-infos/route.ts
@@ -0,0 +1,17 @@
+export async function GET() {
+  const product = {
+    name: "Molho ketchup junior 380g",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_623362-MLA76151912404_052024-O.webp",
+    price: 16.5,
+    isPayable: true,
+    numberOfInstallments: 3,
+    fees: 1,
+    discount: true,
+    discountAmount: 2,
+    description:
+      "O ketchup Junior 380g é um molho de tomate clássico, embalado em uma garrafa prática e fácil de manusear. Ideal para acompanhar uma variedade de pratos, como hambúrgueres, batatas fritas e sanduíches, ele oferece o equilíbrio perfeito entre o doce e o ácido, característico de um ketchup de alta qualidade. A garrafa de 380g é compacta, ideal para famílias menores ou para uso em porções moderadas, garantindo que o produto permaneça fresco por mais tempo. Com uma tampa de fácil abertura e design ergonômico, o ketchup Junior é uma opção conveniente e saborosa para realçar suas refeições diárias.",
+  };
+
+  return Response.json(product);
+}
