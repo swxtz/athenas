83c8a4cb7b496534b157ad1787010db28ee9c207
Rafael Raposo <rrafa7124@gmail.com>
2024-08-28 22:39:14 -0300

    feat: create get purchased products method

--- apps/backend/src/purchased-products/purchased-products.controller.ts
+++ apps/backend/src/purchased-products/purchased-products.controller.ts
@@ -1,9 +1,13 @@
 import { Controller } from "@nestjs/common";
 import { PurchasedProductsService } from "./purchased-products.service";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller("purchased-products")
+@ApiTags("Purchased products")
 export class PurchasedProductsController {
     constructor(
         private readonly purchasedProductsService: PurchasedProductsService,
     ) {}
+
+    @Get 
 }
