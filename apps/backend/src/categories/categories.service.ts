bd3d09009e7e429997ca999dd5e3210089def0dd
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-09-12 21:27:21 -0300

    feat: create get categories endpoint

--- apps/backend/src/categories/categories.service.ts
+++ apps/backend/src/categories/categories.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from "@nestjs/common";
+import { Category } from "./types/category.type";
+import { PrismaService } from "src/prisma/prisma.service";
+
+// interface QueryProducts {
+//     type: {}
+// }
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(private prisma: PrismaService) {}
+
+    private categories: Category[] = [
+        {
+            type: "Pães",
+        },
+        {
+            type: "Molhos",
+        },
+        {
+            type: "Hambugueres",
+        },
+        {
+            type: "Queijos",
+        },
+        {
+            type: "Batatas",
+        },
+    ];
+
+    async getCategories(limit: number) {
+        // Quero que ele retorne 5 produtos de cada categoria
+
+        const products = [];
+
+        for (const category of this.categories) {
+            const categoryProducts = await this.prisma.category.findMany({
+                take: limit || 5,
+                where: {
+                    type: category.type,
+                },
+            });
+
+            products.push({ type: category.type, products: categoryProducts });
+        }
+
+        return products;
+    }
+}
