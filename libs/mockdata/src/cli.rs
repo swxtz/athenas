f1665b2e3655eeafd58f8276520400921d1e088f
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-09-14 01:58:56 -0300

    feat: create clean terminal file

--- libs/mockdata/src/cli.rs
+++ libs/mockdata/src/cli.rs
@@ -1,29 +1,29 @@
-use crate::mock::user::{random_email, random_names, random_password, UserJson};
-use clap::Parser;
-use std::fs::File;
-use std::io::Write;
+
+use clap::{Parser, Subcommand};
+use crate::mock::user::generate_user_mock;
 
 #[derive(Parser)]
-pub struct Cli {
+#[command(about = "Ferramenta de geração de mock data", long_about = None)]
+struct Cli {
+    /// Tipo de mock a ser gerado (ex: user, email)
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    #[command(about = "Gera nomes de usuários aleatórios.", long_about = "Gera uma quantidade especificada de nomes de usuários com identificadores aleatórios.")]
+    User {
     num_size: usize,
+}
 }
 
 pub fn cli() {
-    let args = Cli::parse();
-
-    let mut user_json = Vec::new();
-
-    for _ in 0..args.num_size {
-        user_json.push(UserJson {
-            name: random_names(),
-            email: random_email(),
-            password: random_password(),
-        })
+    let cli = Cli::parse();
+    
+    match &cli.command {
+        Commands::User { num_size } => generate_user_mock(num_size.clone())
     }
 
-    let json = serde_json::to_string_pretty(&user_json).expect("Falha ao serializar para JSON");
 
-    let mut file = File::create("output.json").expect("Falha ao criar o arquivo JSON");
-    file.write_all(json.as_bytes())
-        .expect("Falha ao escrever no arquivo");
 }
