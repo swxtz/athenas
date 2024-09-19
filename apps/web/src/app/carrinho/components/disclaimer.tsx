bd64971038f0ce167295c7a39d55515ec601d2b0
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-09-19 02:27:47 -0300

    feat: create shopping cart disclaimer message

---
+++ apps/web/src/app/carrinho/components/disclaimer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export function Disclaimer() {
+  return (
+    <div className="mt-10">
+      <div className="text-sm text-black/80 bg-zinc-200 border-l-[4px] border-yellow-600 h-14 flex items-center rounded">
+        <p className="ml-4">
+          Ao clicar em &quot;Finalizar compra&quot;, você concorda com os{" "}
+          <Link href="/politica-de-privacidade" className="underline">
+            Política de Privacidade
+          </Link>{" "}
+          e{" "}
+          <Link href="/termos-de-uso" className="underline">
+            Termos de Uso
+          </Link>{" "}
+          da nossa loja.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ apps/web/src/app/carrinho/page.tsx
@@ -0,0 +1,11 @@
+import { Disclaimer } from "./components/disclaimer";
+
+export default function ShoppingCartPage() {
+  return (
+    <div className="container mt-10">
+      <h1 className="font-medium text-2xl">Meu carrinho</h1>
+
+      <Disclaimer />
+    </div>
+  );
+}
