7806c58c9a4569d6ab9237a547efc125148c6504
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-09-23 04:04:52 -0300

    feat: create fonts file

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -11,33 +11,7 @@ import { Navbar } from "@/components/navbar";
 import { ReactQueryProvider } from "@/providers/react-query-providers";
 import { Toaster } from "@/components/ui/toaster";
 import { CartProvider } from "@/context/cart-context";
-
-// fonts
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
-const rubik = Rubik({
-  weight: ["300", "400", "500", "700", "800", "900"],
-  subsets: ["latin"],
-  variable: "--font-rubik",
-});
-const lato = Lato({
-  weight: ["100", "300", "400", "700", "900"],
-  subsets: ["latin"],
-  variable: "--font-lato",
-});
-const montserrat = Montserrat({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-});
-
-const inter = Inter({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+import { inter, lato, montserrat, poppins, rubik } from "./fonts/fonts";
 
 export const metadata: Metadata = {
   title: "RN Distribuidora",
