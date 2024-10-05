cac848955bac164c9b0454082efa6bb43c23df4f
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-10-04 21:36:03 -0300

    chore: change version to 0.0.4

--- apps/backend/src/events/events.controller.ts
+++ apps/backend/src/events/events.controller.ts
@@ -1,18 +1,39 @@
-import { Controller, Header, Req, Sse, UseGuards } from "@nestjs/common";
+import { Controller, Header, Req, Res, Sse, UseGuards } from "@nestjs/common";
 import { EventsService } from "./events.service";
-import { Request } from "express";
+import { Request, Response } from "express";
 import { AuthGuard } from "src/auth/auth.guard";
+import { defer, map, repeat, tap } from "rxjs";
+import { UtilsService } from "src/utils/utils.service";
 
 @Controller("events")
 export class EventsController {
-    constructor(private readonly eventsService: EventsService) {}
+    constructor(
+        private readonly eventsService: EventsService,
+        private utilsService: UtilsService,
+    ) {}
 
     @Sse("get-last-payment-status")
     @Header("Cache-Control", "no-cache")
     @Header("Connection", "keep-alive")
     //@UseGuards(AuthGuard)
-    async getLastPaymentStatus(@Req() req: Request) {
+    async getLastPaymentStatus(@Req() req: Request, @Res() res: Response) {
         req.setTimeout(80000);
-        return this.eventsService.getLastPaymentStatus();
+        return defer(() => this.eventsService.getLastPaymentStatus()).pipe(
+            repeat({
+                delay: 5000,
+            }),
+            tap((payment) => {
+                if (payment.status === "DONE" || payment.status === "FAILED") {
+                    setTimeout(() => {
+                        res.end();
+                    }, 1000);
+                }
+            }),
+            map((payment) => ({
+                id: this.utilsService.createCUID(),
+                type: "payment-status",
+                data: payment,
+            })),
+        );
     }
 }
--- apps/backend/src/events/events.service.ts
+++ apps/backend/src/events/events.service.ts
@@ -1,17 +1,16 @@
-import { Injectable, MessageEvent } from "@nestjs/common";
-import { interval, map, Observable } from "rxjs";
+import { Injectable } from "@nestjs/common";
 import { UtilsService } from "src/utils/utils.service";
 
 @Injectable()
 export class EventsService {
     constructor(private utils: UtilsService) {}
 
-    async getLastPaymentStatus(): Promise<Observable<MessageEvent>> {
-        return interval(100).pipe(
-            map(() => ({
-                //id: this.utils.createCUID(),
-                data: { message: `${this.utils.createCUID()} + buceta` },
-            })),
-        );
+    async getLastPaymentStatus() {
+        return {
+            status: "PENDING",
+            amount: 100,
+        };
     }
+
+    async createPaymentStatus() {}
 }
