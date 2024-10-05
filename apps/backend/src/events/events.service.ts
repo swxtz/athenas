f79f43a2f1a57e2c17c904ce0e114a8ef209b2f6
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-10-04 22:30:33 -0300

    feat: create payment notification table

--- apps/backend/src/events/events.service.ts
+++ apps/backend/src/events/events.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 import { UtilsService } from "src/utils/utils.service";
 
+export enum PaymentStatus {
+    Done = "Done",
+    Failed = "Failed",
+    Pending = "Pending",
+    Refunded = "Refunded",
+    Processing = "Processing",
+}
 @Injectable()
 export class EventsService {
-    constructor(private utils: UtilsService) {}
+    constructor(
+        private utils: UtilsService,
+        private prisma: PrismaService,
+    ) {}
 
-    async getLastPaymentStatus() {
-        return {
-            status: "PENDING",
-            amount: 100,
-        };
+    async getLastPaymentStatus() {}
+    async createPaymentStatus(userId: string) {
+        const paymentStatus = await this.prisma.paymentNotification.create({
+            data: {
+                userId,
+                status: PaymentStatus.Pending,
+                message:
+                    "O Pagamento está pendente. Avisaremos quando for concluído.",
+            },
+        });
+
+        return paymentStatus;
     }
-
-    async createPaymentStatus() {}
 }
