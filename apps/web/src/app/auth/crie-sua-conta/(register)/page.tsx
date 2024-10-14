af6878d28bd12dc81e831310c485778794bf2cc8
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-10-14 03:10:03 -0300

    feat: create logic for render navbar categories

--- apps/web/src/app/auth/crie-sua-conta/(register)/page.tsx
+++ apps/web/src/app/auth/crie-sua-conta/(register)/page.tsx
@@ -9,13 +9,11 @@ export const metadata: Metadata = {
 
 export default function RegisterPage() {
   return (
-
     <div className="w-full bg-slate-50 rounded-lg container mt-9 md:w-1/2 py-11 text-2xl md:flex flex-col ">
       <div className="w-full container text-center flex flex-col gap-4">
         <h2 className="font-semibold container text-x2l">Seja Bem-vindo!</h2>
         <h3 className="font-semibold flex w-full text-xs">Crie sua conta</h3>
         <RegisterForm />
-        
       </div>
     </div>
   );
