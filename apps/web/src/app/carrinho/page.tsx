fd91b1ba00ca323bbd366016e76a8881a5478cd4
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-10-14 15:03:01 -0300

    feat: create mobile disclamer text

--- apps/web/src/app/carrinho/page.tsx
+++ apps/web/src/app/carrinho/page.tsx
@@ -3,7 +3,7 @@ import { Products } from "./components/products";
 
 export default function ShoppingCartPage() {
   return (
-    <div className="container mt-10">
+    <div className="container mt-48 mb-24">
       <h1 className="font-medium text-2xl">Meu carrinho</h1>
 
       <Disclaimer />
