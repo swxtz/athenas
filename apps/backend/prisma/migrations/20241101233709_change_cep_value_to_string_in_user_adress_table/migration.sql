8aa13c4080e26ba38984719fb5ab8eb76a3bcd3d
Rafael Raposo <rrafa7124@gmail.com>
2024-11-01 21:17:15 -0300

    feat: saving the cep also string

---
+++ apps/backend/prisma/migrations/20241101233709_change_cep_value_to_string_in_user_adress_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserAdress" ALTER COLUMN "cep" SET DATA TYPE TEXT;
--- apps/backend/src/cep/cep.service.ts
+++ apps/backend/src/cep/cep.service.ts
@@ -1,3 +1,4 @@
+"use strict";
 import { HttpException, Injectable, Logger } from "@nestjs/common";
 import axios from "axios";
 import { ValidateCepDTO } from "./dtos/validate-cep.dto";
@@ -43,6 +44,7 @@ export class CepService {
 
     async createUserAdress(body: CreateUserAdressDTO, rawtoken) {
         const token = this.utils.removeBearer(rawtoken);
+        console.log(body.cep);
 
         try {
             const jwtpayload: JWTBearerTokenPayLoad =
@@ -84,7 +86,6 @@ export class CepService {
             }
             const useradressdata = {
                 userId: jwtpayload.id,
-                cep: body.cep,
                 city: viacepResponse.data.localidade,
                 street: viacepResponse.data.logradouro,
                 state: viacepResponse.data.estado,
@@ -98,7 +99,7 @@ export class CepService {
                 useradressdata.apBlock = body.apBlock;
             }
             const userAddress = await this.prisma.userAdress.create({
-                data: useradressdata,
+                data: { cep: body.cep, ...useradressdata },
             });
 
             return {
