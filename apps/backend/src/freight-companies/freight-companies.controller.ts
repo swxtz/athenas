c8d8276e32038bf7d1fa4ebceb9d63ac1b1d83c7
Rafael Raposo <rrafa7124@gmail.com>
2024-11-04 16:19:26 -0300

    feat: create send object command in freight companies service

--- apps/backend/src/freight-companies/freight-companies.controller.ts
+++ apps/backend/src/freight-companies/freight-companies.controller.ts
@@ -1,9 +1,41 @@
-import { Controller } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    FileTypeValidator,
+    MaxFileSizeValidator,
+    ParseFilePipe,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+} from "@nestjs/common";
 import { FreightCompaniesService } from "./freight-companies.service";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { CreateFreightCompanyDTO } from "./dtos/create-freight-company.dto";
 
 @Controller("freight-companies")
 export class FreightCompaniesController {
     constructor(
         private readonly freightCompaniesService: FreightCompaniesService,
     ) {}
+
+    @Post("create-freight-company")
+    @UseInterceptors(FileInterceptor("file"))
+    async createFreightCompany(
+        @Body() body: CreateFreightCompanyDTO,
+        @UploadedFile(
+            new ParseFilePipe({
+                validators: [
+                    // new MaxFileSizeValidator({ maxSize: 1000 }),
+                    // new FileTypeValidator({ fileType: "image/jpeg" }),
+                ],
+            }),
+        )
+        file: Express.Multer.File,
+    ) {
+        return this.freightCompaniesService.createFreightCompany(
+            body,
+            file.buffer,
+            file.originalname,
+        );
+    }
 }
