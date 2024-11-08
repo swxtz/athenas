150308920250a3e4f3df5a89fd3a14ccd981285a
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-11-07 21:53:02 -0300

    feat: add signout button

--- apps/web/src/components/menu/desktop-avatar.tsx
+++ apps/web/src/components/menu/desktop-avatar.tsx
@@ -2,18 +2,28 @@
 
 import { useQueryGetPersonalInfo } from "@/hooks/queries/get-personal-info";
 import { Avatar } from "../avatar";
-import { Popover, PopoverTrigger } from "../ui/popover";
-import { useSession } from "next-auth/react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { signOut, useSession } from "next-auth/react";
 import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+
 
 export function DesktopAvatar() {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button className="hover:bg-red-500">
+        <Button className="py-8 px-2" variant={"ghost"}>
           <Avatar />
         </Button>
       </PopoverTrigger>
+      <PopoverContent className="h-fit py-4">
+        <div className="flex items-center justify-center">
+          <Button className="w-[150px] flex gap-1" variant={"destructive"} onClick={() => signOut()}>
+            <LogOut size={18}/>
+            <span>Sair</span>
+          </Button>
+        </div>
+      </PopoverContent>
     </Popover>
   );
 }
