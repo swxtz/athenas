a7c86edb9d8ee4318b331846db07dbab6fa1a493
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-11-26 16:23:30 -0300

    feat: create healthcheck

---
+++ apps/backend/src/healthcheck/healthcheck.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { HealthcheckService } from './healthcheck.service';
+
+@Controller('healthcheck')
+export class HealthcheckController {
+  constructor(private readonly healthcheckService: HealthcheckService) {}
+}
